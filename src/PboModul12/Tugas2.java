676ac36ecef52867d7dfc4d24e7a0ba9e29885e4
ilhmyham <ilhamprogated@gmail.com>
2023-12-13 09:36:58 +0700

    Kurang tugas 2

--- src/PboModul12/Tugas2.java
+++ src/PboModul12/Tugas2.java
@@ -8,8 +8,65 @@ package PboModul12;
  *
  * @author iam
  */
-import javax.swing.*;
 import java.awt.*;
-public class Tugas2 extends javax.swing.Jframe implements java.awt.event.ActionListener{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ *
+ * @author iam
+ */
+public class Tugas2 extends JFrame implements ActionListener{
+    JFrame f;
+    JMenuItem red, yellow, green;
+    JMenuBar barMenu;
+    JMenu wrnMenu;
     
+    public Tugas2(){
+        f = new JFrame("Tugas 2");
+        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        f.setVisible(true);
+        f.setLayout(null);
+        f.setLocationRelativeTo(null);
+        f.setSize(500, 400);
+        
+        barMenu = new JMenuBar();
+        f.setJMenuBar(barMenu);
+        
+        wrnMenu = new JMenu("ganti warna");
+        wrnMenu.setMnemonic(KeyEvent.VK_R);
+        barMenu.add(wrnMenu);
+        
+        red = new JMenuItem("merah");
+        yellow = new JMenuItem("kuning");
+        green = new JMenuItem("hijau");
+        
+        wrnMenu.add(red);
+        wrnMenu.add(green);
+        wrnMenu.add(yellow);
+        
+        red.addActionListener(this);
+        yellow.addActionListener(this);
+        green.addActionListener(this);                       
+        
+    }
+    
+    public void aksi(ActionEvent e){
+        if (e.getSource() == red) {
+            f.getContentPane().setBackground(Color.red);
+        }else if (e.getSource() == yellow) {
+            f.getContentPane().setBackground(Color.yellow);
+        }else if (e.getSource() == green) {
+            f.getContentPane().setBackground(Color.green);
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Tugas2();
+    }    
 }
